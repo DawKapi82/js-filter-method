1a1548f6a0b13344147a951b390db2f21da81a7e
DawKapi82 <dawid_kapi82@hotmail.com>
2023-04-19 10:21:24 +0000

    filters.js

--- filter.js
+++ filter.js
@@ -20,7 +20,11 @@ const people = [
     age: 18,
   },
 ];
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
 
+const paul = people.filter(p => p.age === 18);
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +59,8 @@ const students = [
     ]
   },
 ];
+const has5yearsExp = skill => skill.yrsExperience >=5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+
+const candidates = students.filter(hasStrongSkills);
+  console.log(candidates);
